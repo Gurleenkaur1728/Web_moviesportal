c1238855a17bc8ae138610ea197f5d94540ec9e5
Gurleen Kaur <gillgurleen042@gmail.com>
2025-08-07 20:08:57 -0600

    fixing build errors

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import Navbar from '../components/Navbar.js';
-import Footer from '../components/Footer.js';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 import Link from 'next/link';
 
